10387ce1cb4f8ee9ad922cdfbf9d4d410eeab11e
Chrisathan12 <chris.athan12@gmail.com>
2024-02-06 20:10:23 -0500

    Funcionalidad esTrianguloEquilatero, y tests

--- src/main/java/ec/edu/epn/modelo/Triangulo.java
+++ src/main/java/ec/edu/epn/modelo/Triangulo.java
@@ -14,6 +14,11 @@ public Triangulo(double lado1, double lado2, double lado3) throws TrianguloInval
 this.lado1 = lado1;
 this.lado2 = lado2;
 this.lado3 = lado3;
+//La suma de 2 lados de un tirangulo debe ser mayor que el lado restante
+if (lado1 + lado2 < lado3 || lado2 + lado3 < lado1 || lado3 + lado1 < lado2) {
+        throw new TrianguloInvalidoException("Los lados dados no forman un triangulo");
+}
+
 }
 
 public boolean esEquilatero(){
